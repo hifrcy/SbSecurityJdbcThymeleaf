ee95971a30bf1f955ecc5002c4df88db2c84e22b
Nicolas Luzurier <hifrcy@gmail.com>
2019-01-30 10:42:34 +0100

    ajout du projet complet

---
+++ src/main/java/org/o7planning/SbSecurityJdbcThymeleaf/SbSecurityJdbcThymeleafApplication.java
@@ -0,0 +1,14 @@
+package org.o7planning.SbSecurityJdbcThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurityJdbcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSecurityJdbcThymeleafApplication.class, args);
+	}
+
+}
+
